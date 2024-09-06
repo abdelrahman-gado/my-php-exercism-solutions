acd313006112a5cc80f6eacaf8bc1883e2fc3b34
abdelrahman gado <abdelrahman.magdy.gado@gmail.com>
2024-09-06 23:19:50 +0300

    update List-ops

--- list-ops/ListOps.php
+++ list-ops/ListOps.php
@@ -33,16 +33,7 @@ class ListOps
 
     public function concat(array $list1, array ...$listn): array
     {
-        if (!$listn) {
-            return $list1;
-        }
-
-        $concatenatedList = $list1;
-        foreach ($listn as $list) {
-            array_push($concatenatedList, ...$list);
-        }
-
-        return $concatenatedList;
+        return array_merge($list1, ...$listn);
     }
 
     /**
@@ -87,3 +78,92 @@ class ListOps
         return array_reverse($list);
     }
 }
+
+
+/*
+class ListOps
+{
+    public function append(array $list1, array $list2): array
+    {
+        $newList = $list1;
+        foreach ($list2 as $item) {
+            $newList[] = $item;
+        }
+
+        return $newList;
+    }
+
+    public function concat(array $list1, array ...$listn): array
+    {
+        $newList = $list1;
+        foreach ($listn as $list) {
+            array_push($newList, ...$list);
+        }
+
+        return $newList;
+    }
+
+
+    public function filter(callable $predicate, array $list): array
+    {
+        $filteredList = [];
+        foreach ($list as $item) {
+            if ($predicate($item)) {
+                $filteredList[] = $item;
+            }
+        }
+
+        return $filteredList;
+    }
+
+    public function length(array $list): int
+    {
+        $counter = 0;
+        foreach ($list as $item) {
+            $counter++;
+        }
+
+        return $counter;
+    }
+
+    public function map(callable $function, array $list): array
+    {
+        $newList = [];
+        foreach ($list as $item) {
+            $newList[] = $function($item);
+        }
+
+        return $newList;
+    }
+
+    public function foldl(callable $function, array $list, $accumulator)
+    {
+        $final = $accumulator;
+        foreach ($list as $item) {
+            $final = $function($final, $item);
+        }
+
+        return $final;
+    }
+
+    public function foldr(callable $function, array $list, $accumulator)
+    {
+        $final = $accumulator;
+        for ($i = $this->length($list) - 1; $i >= 0; $i--) {
+            $final = $function($final, $list[$i]);
+        }
+
+        return $final;
+    }
+
+    public function reverse(array $list): array
+    {
+        $reversedList = [];
+        for ($i = $this->length($list) - 1; $i >= 0; $i--) {
+            $reversedList[] = $list[$i];
+        }
+
+        return $reversedList;
+    }
+}
+*/
